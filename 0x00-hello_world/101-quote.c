b6e80b37d579793b7344cd28082c380c780a2999
freemancodes <freemanmusa@gmail.com>
2022-09-08 13:14:38 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 
-/*
+/**
  *  * main - Entry point
  *   *
- *    * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+ *    * Description:'and that piece of art is useful
+ *    " - Dora Korpar, 2015-10-19'
  *     *
  *      * Return: Always 0 (Success)
- *       */
+ */
 
 int main(void)
 {
