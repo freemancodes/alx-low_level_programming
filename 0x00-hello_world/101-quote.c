8e726f61900d9b20ebd64e9c40d34f4e00772d66
freemancodes <freemanmusa@gmail.com>
2022-09-08 13:08:59 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
-/**
+/*
  *  * main - Entry point
  *   *
  *    * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
@@ -11,6 +11,6 @@
 
 int main(void)
 {
-		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-			return (1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
