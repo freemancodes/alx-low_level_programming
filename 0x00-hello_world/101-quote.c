dc9b6f11d47cd30de8d9bb55344fbd36f99b2f1d
freemancodes <freemanmusa@gmail.com>
2022-09-08 13:00:25 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,16 @@
-#!/bin/bash
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+ *  * main - Entry point
+ *   *
+ *    * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+ *     *
+ *      * Return: Always 0 (Success)
+ *       */
+
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-
-	return (1);
+		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+			return (1);
 }
