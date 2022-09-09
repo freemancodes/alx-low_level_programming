680b9099749582a5ff493cc1c8c85fd581713d12
freemancodes <freemanmusa@gmail.com>
2022-09-09 08:00:52 -0700

    print alphabet 3

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,18 @@
+/**
+ * main - entry block
+ * @void: no argument
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char c;
+	char C;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (C = 'A'; C <= 'Z'; C++)
+		putchar(C);
+	putchar('\n');
+	return (0);
+}
