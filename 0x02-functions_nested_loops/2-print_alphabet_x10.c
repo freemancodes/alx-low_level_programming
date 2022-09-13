ca136557c90ed5a1707254baf6add834cdb773c3
freemancodes <freemanmusa@gmail.com>
2022-09-13 05:04:59 -0700

    aphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print alphabet in lowercase 10 times
+ *
+ * Return: Always 0
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char c;
+
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
