48deb0ab59aa4f1964ddbfa7e8130c21d9143997
freemancodes <freemanmusa@gmail.com>
2022-09-19 03:28:37 -0700

    pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string, in reverse
+ *
+ * @s: char
+ *
+ * Return: string
+ *
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	char c;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+	}
+	for (i = i - 1; i >= 0; i--)
+	{
+		c = s[i];
+		_putchar(c);
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s:a pointer to a char
+ *
+ * Return: length of a string
+ *
+ */
+
+int _strlen(char *s)
+{
+	int l, i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	l = i;
+	return (l);
+}
+
+/**
+ * puts2 - prints one char out of two in a string
+ *
+ * @str: a pointer to a string
+ *
+ * Return: nothing
+ *
+ */
+
+void puts2(char *str)
+{
+	int i;
+	int size = _strlen(str);
+
+	for (i = 0; i < size; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
