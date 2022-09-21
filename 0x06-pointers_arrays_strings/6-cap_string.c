d180a8999ed37d93b4fee55d069c322d711c7d7c
freemancodes <freemanmusa@gmail.com>
2022-09-21 13:40:25 -0700

    cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,30 +1,36 @@
 #include "main.h"
-
 /**
- * *cap_string - a function that capitalizes all words of a string
- * @str: string
- * Return: string
+ *cap_string - capitalizes every first letter of a word in a string.
+ *separators of words are:  space, tabulation,
+ * new line, ,, ;, ., !, ?, ", (, ), {, and }.
+ *@s: pointer to string.
+ *
+ *Return: pointer to s.
  */
-
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
-	int i = 1;
+	int count;
 
-	if (str[0] >= 'a' && str[0] <= 'z')
-		str[0] = str[0] - 32;
-
-	while (str[i] != '\0')
-	{
-                if  ((str[i - 1] == ' ' || str[i - 1] == '\t'
-			|| str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';'
-			|| str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?'
-			|| str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')'
-			|| str[i - 1] == '{' || str[i - 1] == '}')
-			&& (str[i] >= 'a' && str[i] <= 'z'))
+/*  scan through string */
+	count = 0;
+	while (s[count] != '\0')
+	{/* if next character after count is a char , capitalise it */
+		if (s[0] >= 97 && s[0] <= 122)
 		{
-				str[i] = str[i] - 32;
+			s[0] = s[0] - 32;
 		}
-		i++;
+		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
+		    || s[count] == ',' || s[count] == ';' || s[count] == '.'
+		    || s[count] == '.' || s[count] == '!' || s[count] == '?'
+		    || s[count] == '"' || s[count] == '(' || s[count] == ')'
+		    || s[count] == '{' || s[count] == '}')
+		{
+			if (s[count + 1] >= 97 && s[count + 1] <= 122)
+			{
+				s[count + 1] = s[count + 1] - 32;
+			}
+		}
+		count++;
 	}
-	return (str);
+	return (s);
 }
