d0eda86fe6df0ce590930137452c32e6a939d76a
freemancodes <freemanmusa@gmail.com>
2022-09-22 12:06:30 -0700

    infinite add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,45 +1,70 @@
 #include "main.h"
+
 /**
- * infinite_add - adds two numbers
- * @n1: first number
- * @n2: second number
- * @r: buffer for result
- * @size_r: buffer size
- *
- * Return: address of r or 0
+ * _strlen - calculate string length
+ * @a: string
+ * Return: length of string
  */
+
+int _strlen(char *a)
+{
+	int i = 0;
+
+	while (*a != '\0')
+		i++, a++;
+	return (i);
+}
+
+/**
+ * *infinite_add - adds two numbers
+ * @n1: string
+ * @n2: string
+ * @r: string
+ * @size_r: int
+ *
+ * @n1: number1
+ * @n2: number2
+ * @r: buffer to store result
+ * @size_r: buffer size
+ * Return: pointer to the result
+ */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int i, j, k, l, m, n;
-for (i = 0; n1[i]; i++)
-;
-for (j = 0; n2[j]; j++)
-;
-if (i > size_r || j > size_r)
-return (0);
-m = 0;
-for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
-{
-n = m;
-if (i >= 0)
-n += n1[i] - '0';
-if (j >= 0)
-n += n2[j] - '0';
-if (i < 0 && j < 0 && n == 0)
-{
-break;
-}
-m = n / 10;
-r[k] = n % 10 + '0';
-}
-r[k] = '\0';
-if (i >= 0 || j >= 0 || m)
-return (0);
-for (k -= 1, l = 0; l < k; k--, l++)
-{
-m = r[k];
-r[k] = r[l];
-r[l] = m;
-}
-return (r);
+	int k, n2_s, n1_s, m;
+
+	r[size_r] = '\0';
+	k = 0;
+	m = 0;
+	n1_s = _strlen(n1);
+	n2_s = _strlen(n2);
+	size_r--;
+	if (n1_s > size_r || n2_s > size_r || size_r == 0 ||
+		(n1_s == size_r && n2_s == size_r &&
+		 (n1[0] - '0') + (n2[0] - '0') > 9))
+	{
+		return (0);
+	}
+	n1_s--;
+	n2_s--;
+	while (n1_s >= 0 || n2_s >= 0)
+	{
+		if (n1_s < 0)
+			m = n2[n2_s] - '0' + k;
+		else if (n2_s < 0)
+			m = n1[n1_s] - '0' + k;
+		else
+			m = (n1[n1_s] - '0') + (n2[n2_s] - '0') + k;
+		r[size_r] = (m % 10) + '0';
+		k = m / 10;
+		n1_s--, n2_s--, size_r--;
+	}
+	if (size_r > 0 && (k < 9 && k > 0))
+	{
+		r[size_r] = k + '0';
+		return (r + size_r);
+	}
+	else if (k == 0)
+		return (r + size_r);
+	return (0);
 }
