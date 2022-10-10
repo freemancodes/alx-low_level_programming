b93eba319d47e42838130544d47cd103754021a3
freemancodes <freemanmusa@gmail.com>
2022-10-10 10:05:34 -0700

    free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+* free_dog - a function that frees dogs
+* @d: pointer to struct
+* Return: nothing
+*/
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
