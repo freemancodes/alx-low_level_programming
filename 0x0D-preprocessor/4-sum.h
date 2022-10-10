e9067b06c3b774700a62f1232f7330581751ae64
freemancodes <freemanmusa@gmail.com>
2022-10-10 09:53:07 -0700

    sum

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,4 +1,12 @@
-#ifndef ABS_H
-#define ABS_H
-#define ABS(x) (((x) < 0) ? -(x) : (x))
+#ifndef SUM_H
+#define SUM_H
+
+/*
+ * File: 4-sum.h
+ * Desc: Header file that defines a function-like macro SUM(x, y)
+ *       that computes the sum of the numbers x and y.
+ */
+
+#define SUM(x, y) ((x) + (y))
+
 #endif
