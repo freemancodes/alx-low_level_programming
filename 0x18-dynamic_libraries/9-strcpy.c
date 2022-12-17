66bb92cde9b99ac89e221fd29f5aae188434b991
freemancodes <freemanmusa@gmail.com>
2022-12-16 18:59:17 -0800

    dynamic libraries

---
+++ 0x18-dynamic_libraries/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - entry point
+ * Description: copies the string pointer, null byte, dest pointer
+ * @dest: destination
+ * @src: font
+ * Return: dest pointer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	for (; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+		j++;
+	return (dest);
+}
