08a49d2c693bec97d27bdd893182485398e09fdd
freemancodes <freemanmusa@gmail.com>
2022-12-16 20:16:49 -0800

    operations

---
+++ 0x18-dynamic_libraries/operation.h
@@ -0,0 +1,11 @@
+#ifndef _OPERATION_H_
+#define _OPERATION_H_
+
+int add(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+int sub( int a, int b);
+
+
+#endif    /* OPERATION_H_ */
